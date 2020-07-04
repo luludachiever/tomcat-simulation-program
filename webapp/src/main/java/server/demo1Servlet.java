786a4d08006593c4cf47bf648d22e74cc5e417f1
luludachiever <luludachiever@gmail.com>
2020-07-04 16:49:06 -0700

    add

---
+++ webapp/src/main/java/server/demo1Servlet.java
@@ -0,0 +1,39 @@
+package server;
+
+import java.io.IOException;
+public class demo1Servlet extends HttpServlet {
+    @Override
+    public void doGet(Request request, Response response) {
+        try {
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String content = "<h1>demo1 get</h1>";
+        try {
+            response.output((HttpProtocolUtil.getHttpHeader200(content.getBytes().length) + content));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void doPost(Request request, Response response) {
+        String content = "<h1>demo1 post</h1>";
+        try {
+            response.output((HttpProtocolUtil.getHttpHeader200(content.getBytes().length) + content));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+
+    }
+
+    @Override
+    public void destory() throws Exception {
+
+    }
+}
